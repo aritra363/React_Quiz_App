c0c6ce608e4c79c282bb38d13f7209fc03919454
aritra363 <aritrapaul363@gmail.com>
2020-04-14 18:46:03 +0530

    Updates

---
+++ precache-manifest.685fd53f6588d0fb55649b0973260615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcd4fdfaed4001bf00aa78e2764d4e9",
+    "url": "/React_Quiz_App/index.html"
+  },
+  {
+    "revision": "c14697e811f7ed8f5500",
+    "url": "/React_Quiz_App/static/css/main.3b8751c9.chunk.css"
+  },
+  {
+    "revision": "9558c68fe60f6aea806a",
+    "url": "/React_Quiz_App/static/js/2.fe2f12a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Quiz_App/static/js/2.fe2f12a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14697e811f7ed8f5500",
+    "url": "/React_Quiz_App/static/js/main.9214364a.chunk.js"
+  },
+  {
+    "revision": "502c97b66d5323dd9191",
+    "url": "/React_Quiz_App/static/js/runtime-main.678a35e8.js"
+  }
+]);
